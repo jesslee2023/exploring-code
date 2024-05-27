f3bc7dc93efceaa98ef4435a27696fd4ee15d951
osmankelifa <145119027+osmankelifa@users.noreply.github.com>
2024-05-27 21:00:48 +0200

    first commit. background + navbar

--- src/components/osman/Hero.jsx
+++ src/components/osman/Hero.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import "./Hero.css";
+import bg3 from "../../assets/bg3.png"
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return <>
+  <header className="header">
+    <a href="/" className="logo">Logo</a>
+
+    <nav className="navbar">
+    <a href="/">Osman</a>
+    <a href="/">Moysis</a>
+    <a href="/">Jess</a>
+    <a href="/">Musab</a>
+    <a href="/">Hifza</a>
+    <a href="/">Samuel</a>
+    </nav>
+  </header>
+  <body className="hero">
+    <img src={bg3}></img>
+  
+  </body>
+  </>;
 };
 
 export default Hero;
